5218a86281ce238503e1a4563a7171f8d91883f7
chrismadlax <chrismadlax@gmail.com>
2025-05-16 22:33:20 +0800

    .net event callback down to LV18

---
+++ dotNETCallback/dotNETCallbackPrjLV18/LVcallback/myClass2/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+//定義域名供LabVIEW識別
+namespace BLECallbackDemo
+{
+    // 定義事件參數型別
+    public class BLEDataEventArgs : EventArgs
+    {
+        public string Data { get; }
+
+        public BLEDataEventArgs(string data)
+        {
+            Data = data;
+        }
+    }
+
+    // 定義 BLE 管理類別
+    public class BLEManager
+    {
+        //宣告事件供外部語言訂閱
+        public event EventHandler<BLEDataEventArgs> DataReceived;
+
+        //模擬接收行為
+        public void SimulateReceive(string data)
+        {
+            OnDataReceived(new BLEDataEventArgs(data));
+        }
+        
+        //觸發callback事件
+        protected virtual void OnDataReceived(BLEDataEventArgs e)
+        {
+            DataReceived?.Invoke(this, e);
+        }
+    }
+}
